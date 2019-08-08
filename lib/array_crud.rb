c195b2b321e75420cd2618de24a774d5b9d382ab
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-08 02:32:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,7 +42,13 @@ end
 
 def remove_element_from_start_of_array(array)
 
-  
+shift = ["cat", "dog", "bird"]
+cat = animals.shift
+ 
+p animals
+#=> ["cat","dog"]
+p bird
+#=> lil' bub  
 end
 
 def retrieve_element_from_index(array, index_number)
